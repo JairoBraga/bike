5a99099e1680a3265caf715c2de4223ccce19f8c
Jairo Braga <neto.braga08@gmail.com>
2022-08-21 14:21:48 -0300

    fix: Correção do arquivo de privilegios

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x /home/ec2-user/bike-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ec2-user/start_application.sh
+chmod +x /home/ec2-user/stop_application.sh
